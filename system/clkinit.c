e1c812c45836316e0fe8f9b426737159660644aa
bibin <bibinkurian2@gmail.com>
2019-09-05 19:41:17 -0400

    added ctr1000 to header file

--- system/clkinit.c
+++ system/clkinit.c
@@ -3,7 +3,7 @@
 #include <xinu.h>
 
 uint32	clktime;		/* Seconds since boot			*/
-uint32	ctr1000 = 0;		/* Milliseconds since boot		*/
+uint32	ctr1000;		/* Milliseconds since boot		*/
 qid16	sleepq;			/* Queue of sleeping processes		*/
 uint32	preempt;		/* Preemption counter			*/
 
@@ -24,6 +24,7 @@ void	clkinit(void)
 	preempt = QUANTUM;
 
 	/* Initialize the time since boot to zero */
+	ctr1000=0;
 
 	clktime = 0;
 
