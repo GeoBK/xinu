a774195b8046e0533f3f0d566e0071c0c0e48725
bibin <bibinkurian2@gmail.com>
2019-09-10 00:39:37 -0400

    changes to all system calls

--- system/kill.c
+++ system/kill.c
@@ -10,6 +10,16 @@ syscall	kill(
 	  pid32		pid		/* ID of process to kill	*/
 	)
 {
+	unsigned long num_cycles;
+	unsigned long start, end;
+	unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
+	unsigned long flags;	
+	asm volatile ("CPUID\n\t"
+					 "RDTSC\n\t"
+					 "mov %%edx, %0\n\t"
+					 "mov %%eax, %1\n\t": "=r" (cycles_high), "=r"
+					(cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
+	//-------------------------------------------------------------
 	intmask	mask;			/* Saved interrupt mask		*/
 	struct	procent *prptr;		/* Ptr to process's table entry	*/
 	int32	i;			/* Index into descriptors	*/
@@ -18,6 +28,24 @@ syscall	kill(
 	if (isbadpid(pid) || (pid == NULLPROC)
 	    || ((prptr = &proctab[pid])->prstate) == PR_FREE) {
 		restore(mask);
+		asm volatile("RDTSCP\n\t"
+				 "mov %%edx, %0\n\t"
+				 "mov %%eax, %1\n\t"
+				 "CPUID\n\t": "=r" (cycles_high1), "=r"
+				(cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx");	
+		start = ( ((long)cycles_high << 32) | cycles_low );
+		end = ( ((long)cycles_high1 << 32) | cycles_low1 );	
+		int len= sizeof(long);
+		if ( (end - start) < 0) {
+ 			printf("\n\n>>>>>>>>>>>>>> CRITICAL ERROR IN TAKING TIME!!!!!!\n start = %llu, end = %llu, \n",  start, end);
+ 			num_cycles = 0;
+ 		}
+ 		else
+ 		{
+ 			num_cycles = end - start;
+ 		}
+		procsumm_table[pid].rec_count[create_enum]++;
+		procsumm_table[pid].total_cycles[create_enum]+=(int)num_cycles;
 		return SYSERR;
 	}
 
@@ -52,8 +80,25 @@ syscall	kill(
 
 	default:
 		prptr->prstate = PR_FREE;
-	}
-
+	}		
 	restore(mask);
+	asm volatile("RDTSCP\n\t"
+				 "mov %%edx, %0\n\t"
+				 "mov %%eax, %1\n\t"
+				 "CPUID\n\t": "=r" (cycles_high1), "=r"
+				(cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx");	
+	start = ( ((long)cycles_high << 32) | cycles_low );
+	end = ( ((long)cycles_high1 << 32) | cycles_low1 );	
+	int len= sizeof(long);
+	if ( (end - start) < 0) {
+ 		printf("\n\n>>>>>>>>>>>>>> CRITICAL ERROR IN TAKING TIME!!!!!!\n start = %llu, end = %llu, \n",  start, end);
+ 		num_cycles = 0;
+ 	}
+ 	else
+ 	{
+ 		num_cycles = end - start;
+ 	}
+	procsumm_table[pid].rec_count[kill_enum]++;
+	procsumm_table[pid].total_cycles[kill_enum]+=(int)num_cycles;
 	return OK;
 }
