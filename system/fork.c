912344c73f0fd9b19f66eeebb67d3ed8af678fb8
bibin <bibinkurian2@gmail.com>
2019-09-11 12:10:50 -0400

    parantheses

--- system/fork.c
+++ system/fork.c
@@ -61,7 +61,7 @@ pid32 fork(){
     uint32 *savsp;
     savsp = (uint32*) proctab[child_pid].prstkbase-stack_length+1;		/* Start of frame for ctxsw	*/
     uint32 *saddr;
-    saddr=(uint32*) proctab[child_pid].prstkbase-stack_length+1;
+    saddr=(uint32*) (proctab[child_pid].prstkbase-stack_length+1);
     kprintf("prstkbase: %0X \n", proctab[child_pid].prstkbase);
     kprintf("proctab[child_pid].prstkbase-stack_length+1: %0X \n", proctab[child_pid].prstkbase-stack_length+1);
     kprintf("saddr: %0X \n", saddr);
