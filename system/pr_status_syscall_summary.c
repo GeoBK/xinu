c7ac4d3e6791721370e4cb2332a9b35658e49d54
bibin <bibinkurian2@gmail.com>
2019-09-12 15:15:14 -0400

    change

--- system/pr_status_syscall_summary.c
+++ system/pr_status_syscall_summary.c
@@ -19,9 +19,9 @@ static char sys_call_description[number_of_system_calls][7] =
     "yield"
 };
 void pr_status_syscall_summary(){
-    int32 i,j,k;
+    int32 i,j;
     //TODO: make this wait on a semaphore for console out temporary hack 
-    for(k=0;k<1000000;k++);
+    
 
     printf("%3s %7s %5s %14s\n",
 		   "pid", "syscall", "count", "average cycles");
@@ -29,12 +29,12 @@ void pr_status_syscall_summary(){
 	printf("%3s %7s %5s %14s\n",
 		   "---", "-------", "-----", "--------------");    
     
-    for(i=0;i<=max_pid;i++){        
+    for(i=1;i<=max_pid;i++){        
         for(j=create_enum; j< number_of_system_calls; j++){
             if(procsumm_table[i].rec_count[j]!=0){
                 double average_cycles= procsumm_table[i].total_cycles[j]/procsumm_table[i].rec_count[j];
                 kprintf("total cycles %f \n",procsumm_table[i].total_cycles[j]);
-                printf("%3d %7s %5d %d\n",
+                printf("%3d %7s %5d %f\n",
                         i, sys_call_description[j],procsumm_table[i].rec_count[j],average_cycles);
                 
             }                   
