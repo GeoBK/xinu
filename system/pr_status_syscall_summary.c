a43d211e8964e995bbaedd5a3651aa857722c133
bibin <bibinkurian2@gmail.com>
2019-09-12 15:56:34 -0400

    debugged

--- system/pr_status_syscall_summary.c
+++ system/pr_status_syscall_summary.c
@@ -36,7 +36,10 @@ void pr_status_syscall_summary(){
     for(i=1;i<=max_pid;i++){        
         for(j=create_enum; j< number_of_system_calls; j++){
             if(procsumm_table[i].rec_count[j]!=0){
-                double average_cycles= procsumm_table[i].total_cycles[j]/(double)procsumm_table[i].rec_count[j];
+                double average_cycles= (double)procsumm_table[i].total_cycles[j]/(double)procsumm_table[i].rec_count[j];
+                kprintf("Long int value: %u \n", procsumm_table[i].total_cycles[j]);
+                kprintf("double value: %f \n", (double)procsumm_table[i].total_cycles[j]);
+                kprintf("average cycles: %f \n", average_cycles);
                 kprintf("total cycles %f \n",(double)procsumm_table[i].total_cycles[j]/(double)procsumm_table[i].rec_count[j]);
                 printf("%3d %7s %5d %14f\n",
                         i, sys_call_description[j],procsumm_table[i].rec_count[j],average_cycles);                
