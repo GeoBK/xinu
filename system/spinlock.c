3790c98198f4a4744b2b55191e3c97f0cc13ae3e
bibin <bibinkurian2@gmail.com>
2019-10-23 16:09:48 -0400

    spinlocks

--- system/spinlock.c
+++ system/spinlock.c
@@ -3,12 +3,15 @@
 syscall sl_initlock(sl_lock_t *l)
 {
     l->flag=0;
+    return OK;
 }
 syscall sl_lock(sl_lock_t *l)
 {
     while(test_and_set((void*)&l->flag,1)==1);
+    return OK;
 }
 syscall sl_unlock(sl_lock_t *l)
 {
     l->flag=0;
+    return OK;
 }
