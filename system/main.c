6233a25b1561286e8389d280ccf6fd0215520c2a
bibin <bibinkurian2@gmail.com>
2019-11-12 13:52:21 -0500

    deso

--- system/main.c
+++ system/main.c
@@ -164,6 +164,15 @@
 
 #include <xinu.h>
 
+void sync_printf(char *fmt, ...)
+
+{    
+	intmask mask = disable();
+	void *arg = __builtin_apply_args();
+	__builtin_apply((void*)kprintf, arg, 100);
+	restore(mask);
+}
+
 process cycliclockswithtrylock(pi_lock_t *l1)
 {
 	
