30e577147ae3a9600267bac598c1f1d4454fae5a
bibin <bibinkurian2@gmail.com>
2019-11-12 21:53:43 -0500

    deso

--- system/main.c
+++ system/main.c
@@ -196,6 +196,16 @@
 
 #include <xinu.h>
 
+// void sync_printf(char *fmt, ...)
+
+{    
+	intmask mask = disable();
+	void *arg = __builtin_apply_args();
+	__builtin_apply((void*)kprintf, arg, 100);
+	restore(mask);
+}
+
+
 inline uint32 get_timestamp(){
 	return ctr1000;
 }
