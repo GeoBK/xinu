f295e3bca8d0ee21e2659c74760c48de2cef591d
bibin <bibinkurian2@gmail.com>
2019-11-12 21:15:53 -0500

    deso

--- system/main.c
+++ system/main.c
@@ -176,8 +176,8 @@ process main()
 
 	//Testing transitivity of priority inheritance
 	pi_lock_t l7,l8;
-	initlock(&l7);	
-	initlock(&l8);
+	pi_initlock(&l7);	
+	pi_initlock(&l8);
 	pid32 pid7 = create((void *)multlocks, INITSTK, 1,"ml", 2, &l7, &l8);
 	pid32 pid8 = create((void *)singlelock, INITSTK, 1,"ml", 3, &l7);
 	pid32 pid9 = create((void *)singlelock, INITSTK, 1,"ml", 4, &l8);
