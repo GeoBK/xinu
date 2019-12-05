44f570b0a85d9a6e9ccb995c7ddec21b767a751f
bibin <bibinkurian2@gmail.com>
2019-12-04 22:52:04 -0500

    athoke thae

--- system/create.c
+++ system/create.c
@@ -49,7 +49,7 @@ pid32	create(
 	prptr->prsem = -1;
 	prptr->prparent = (pid32)getpid();
 	prptr->prhasmsg = FALSE;
-	prptr->pdbr = (char*)XINU_PAGES * PAGE_SIZE;
+	prptr->pdbr = (char*)(XINU_PAGES * PAGE_SIZE);
 
 	/* Set up stdin, stdout, and stderr descriptors for the shell	*/
 	prptr->prdesc[0] = CONSOLE;
