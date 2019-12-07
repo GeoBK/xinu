7c95174a27d49af3281e133eeac853eadd93c3a1
bibin <bibinkurian2@gmail.com>
2019-12-07 01:54:40 -0500

    deso

--- system/freemem.c
+++ system/freemem.c
@@ -167,7 +167,7 @@ syscall	vfree(char * addr, uint32 size)
 		if(pd[pd_index].pd_pres==1)
 		{
 			pt_t *pt=(pt_t*)(pd[pd_index].pd_base<<12);
-			
+			kprintf("Freeing pd_index: %d, pt_index: %d\n",pd_index,pt_index);
 			pt[pt_index].pt_valid=0;
 			pt[pt_index].pt_pres=0;
 			pt[pt_index].pt_write=0;
